f9a1618f93a05a868d2b0f7744c09c558036d9d5
Greg Reckenwald <gr536@drexel.edu>
2025-04-13 11:10:24 -0600

    Updated comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,15 @@
  * <ul>
  * <li>1) Hard coded 1 freight, 1 guest, and 1 service elevator. If we were to
  * use this solution for multiple buildings that may have different number of
- * elevators, then I would use an XML or JSON configuration file to define the
+ * elevators, then I would use a XML or JSON configuration file to define the
  * elevators for each implementation. Or implement an Admin GUI.</li>
  * <li>2) Floor requests only accept integers and do not account for basement or
  * "B" or penthouse "P" values.</li>
  * <li>3) Move to floor is only FIFO. For example, elevator won't stop on floor
  * 8 if moving to floor 10. Could be optimized.</li>
  * <li>4) There are no external up / down buttons for floors.</li>
+ * <li>5) Console driven. Could add a GUI for simulation.</li>
+ * <li>6) No junit test scripts.</li>
  * </ul>
  * </p>
  * <p>
